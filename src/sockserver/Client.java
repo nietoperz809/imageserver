892184e8bdaacc3b6c6ec297a006b3a6b79f3942
pittbull <pbull809@gmail.com>
2015-05-06 23:14:13 +0200

    Merge origin/master
    Conflicts:
    	pdf_template.php

--- src/sockserver/Client.java
+++ src/sockserver/Client.java
@@ -9,8 +9,12 @@
 package sockserver;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.logging.Level;
@@ -24,6 +28,7 @@ public class Client implements Runnable
 {
     Socket m_sock;
     Thread m_thread;
+    private final String PATH = "F:/feet/";
     
     public Client(Socket s)
     {
@@ -47,9 +52,62 @@ public class Client implements Runnable
         out.print (txt);
     }
     
+    protected String listFiles()
+    {
+        File f = new File (PATH);
+        StringBuilder sb = new StringBuilder();
+        File[] fils = f.listFiles();
+        
+        if (fils == null)
+            return "noone";
+        
+        for (int n=0; n<fils.length; n++)
+        {
+            String name = fils[n].getName();
+            if (name.endsWith(".jpg") == false)
+                continue;
+            sb.append ("<img src=\"");
+            sb.append (name);
+            sb.append ("\" alt=\"Smiley face\" height=\"256\" width=\"256\">\r\n");
+        }
+        return sb.toString();
+    }
+    
+    protected void image (OutputStream out, String fname)
+    {
+        File f = new File (PATH+fname);
+        PrintWriter w = new PrintWriter(out);
+        w.println ("HTTP/1.1 200 OK");
+        w.println ("Content-Length: "+f.length());
+        w.println ("Content-Type: image/jpeg");
+        w.println ();
+        w.flush();
+        try
+        {
+            InputStream input = new FileInputStream(f);
+            byte[] b = new byte[(int)f.length()];
+            input.read(b);
+            out.write(b);
+            out.flush();
+        }
+        catch (Exception ex)
+        {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    protected void imagePage (PrintWriter out)
+    {
+        String txt = "<html>\r\n"+listFiles()+"</html>";
+        sendHeader (out, txt, "text/html");
+        out.print (txt);
+    }
+    
     private String[] getInput (BufferedReader in) throws IOException
     {
-        return in.readLine().split (" ");
+        String[] out =  in.readLine().split (" ");
+        out[1] = java.net.URLDecoder.decode(out[1], "UTF-8");
+        return out;
     }
     
     private void naked (PrintWriter out, String txt)
@@ -71,6 +129,16 @@ public class Client implements Runnable
             {
                 case "/":
                 html (out, "hello wÖrldx");
+                break;
+                
+                case "/img":
+                imagePage (out);
+                break;
+                
+                default:
+                String fname = si[1].substring(1);
+                image (m_sock.getOutputStream(), fname);
+                //image (new PrintWriter(System.out), fname);
                 break;
                     
                 case "/stop":
