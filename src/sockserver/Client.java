b6ddaf79cc1a4ea4c0c280c4930f6c06ba6ef5ac
pittbull <pbull809@gmail.com>
2015-05-08 08:48:39 +0200

    save

--- src/sockserver/Client.java
+++ src/sockserver/Client.java
@@ -17,7 +17,10 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
 import static sockserver.Sockserver.pStr;
 import static sockserver.Sockserver.PATH;
 
@@ -37,40 +40,42 @@ public class Client implements Runnable
         m_thread.start();
     }
 
-    private BufferedImage resizeImage(BufferedImage originalImage)
+    private BufferedImage resizeImage (BufferedImage originalImage, int b, int w)
     {
         int type = originalImage.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : originalImage.getType();
-        BufferedImage resizedImage = new BufferedImage(100, 100, type);
+        BufferedImage resizedImage = new BufferedImage(b, w, type);
         Graphics2D g = resizedImage.createGraphics();
-        g.drawImage(originalImage, 0, 0, 100, 100, null);
+        g.drawImage(originalImage, 0, 0, b, w, null);
         g.dispose();
         return resizedImage;
-    }
-
-    private byte[] reduceImg(File path) throws Exception
-    {
-        BufferedImage image = ImageIO.read(path);
-        BufferedImage i2 = resizeImage(image);
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        ImageIO.write(i2, "jpg", os);
-        return os.toByteArray();
     }
 
 //    private byte[] reduceImg(File path) throws Exception
 //    {
 //        BufferedImage image = ImageIO.read(path);
+//        BufferedImage i2 = resizeImage(image);
 //        ByteArrayOutputStream os = new ByteArrayOutputStream();
-//        ImageWriter writer = (ImageWriter) ImageIO.getImageWritersByFormatName("jpeg").next();
-//
-//        ImageWriteParam param = writer.getDefaultWriteParam();
-//        param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-//        param.setCompressionQuality(0.2f); // Change this, float between 0.0 and 1.0
-//
-//        writer.setOutput(ImageIO.createImageOutputStream(os));
-//        writer.write(null, new IIOImage(image, null, null), param);
-//        writer.dispose();
+//        ImageIO.write(i2, "jpg", os);
 //        return os.toByteArray();
 //    }
+
+    private byte[] reduceImg(File path) throws Exception
+    {
+        BufferedImage image = ImageIO.read(path);
+        image = resizeImage (image, 100, 100);
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        ImageWriter writer = (ImageWriter) ImageIO.getImageWritersByFormatName("jpeg").next();
+
+        ImageWriteParam param = writer.getDefaultWriteParam();
+        param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+        param.setCompressionQuality(0.2f); // Change this, float between 0.0 and 1.0
+
+        writer.setOutput(ImageIO.createImageOutputStream(os));
+        writer.write(null, new IIOImage(image, null, null), param);
+        writer.dispose();
+        return os.toByteArray();
+    }
+    
     private void sendHeader(PrintWriter out, String txt, String type)
     {
         out.println("HTTP/1.1 200 OK");
@@ -110,7 +115,7 @@ public class Client implements Runnable
             {
                 dirs.add(name);
             }
-            else if (name.endsWith(".jpg"))
+            else if (name.toLowerCase().endsWith(".jpg"))
             {
                 sb.append("<a href=\"");
                 sb.append("*IMG*");
